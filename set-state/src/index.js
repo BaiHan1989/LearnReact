113abfc5d386f6286fe493df2a57594bc2dab92b
Lazyload <baih_1989@163.com>
2020-11-18 11:08:15 +0800

    this.state不可变性

--- set-state/src/index.js
+++ set-state/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './01_async-update'; // setState 异步更新
 // import App from './02_sync-update' // setState 同步更新
-import App from './03_immutable-data' // setState 中数据的不可变性
+import App from './03_immutable-data' // this.state 中数据的不可变性
 
 ReactDOM.render(
   <App />,
