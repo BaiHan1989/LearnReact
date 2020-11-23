a49d2ef6b66148423afe78bc37163868196288ff
Lazyload <baih_1989@163.com>
2020-11-23 14:09:51 +0800

    portals的使用

--- higher-order-components/src/index.js
+++ higher-order-components/src/index.js
@@ -7,7 +7,10 @@ import ReactDOM from 'react-dom';
 // import App from './04_login-auth' // 高阶组件应用 - 登录鉴权
 // import App from './05_life-circle' // 高阶组件的应用 - 生命周期劫持
 
-import App from './06_forward-ref' // forwardRef 高阶组件的使用
+// import App from './06_forward-ref' // forwardRef 高阶组件的使用
+
+import App from './07_portals' // portals 的使用
+import './index.css'
 
 ReactDOM.render(
   <App name="Hello React" />,
