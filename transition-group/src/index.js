85e96d34a6e44b1a35de2b19ee1c0e444e4d2814
Lazyload <baih_1989@163.com>
2020-12-03 14:02:03 +0800

    CSSTtransition的使用

---
+++ transition-group/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+import 'antd/dist/antd.css'
+
+import './transition/CSSTransition.css'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
