13ba08d36e4122c8d4180ecf832ed77c1f4a88ba
Lazyload <baih_1989@163.com>
2020-12-09 14:21:12 +0800

    generator的学习

--- react-redux/src/store/index.js
+++ react-redux/src/store/index.js
@@ -1,12 +1,15 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 
 import thunkMiddleware from "redux-thunk"
 
 import reducer from './reducer'
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({trace: true}) || compose;
+
 // 应用中间件
 const storeEnhancer = applyMiddleware(thunkMiddleware)
 
-const store = createStore(reducer, storeEnhancer)
+const store = createStore(reducer, composeEnhancers(storeEnhancer))
 
 export default store
