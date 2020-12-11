39122ab12369733d2370c3e43c2363e803184d8d
Lazyload <baih_1989@163.com>
2020-12-11 16:56:15 +0800

    combineReducers的使用

--- redux-reducer/src/store/reducer.js
+++ redux-reducer/src/store/reducer.js
@@ -2,12 +2,20 @@
 import { reducer as counterReducer } from './counter'
 import { reducer as homeReducer } from './home'
 
+import { combineReducers } from 'redux'
+
 // 创建reducer
-const reducer = (state = {}, action) => {
-  return {
-    counterInfo: counterReducer(state.counterInfo, action),
-    homeInfo: homeReducer(state.homeInfo, action)
-  }
-}
+// const reducer = (state = {}, action) => {
+//   return {
+//     counterInfo: counterReducer(state.counterInfo, action),
+//     homeInfo: homeReducer(state.homeInfo, action)
+//   }
+// }
+
+// 合并reducer
+const reducer = combineReducers({
+  counterInfo: counterReducer,
+  homeInfo: homeReducer
+})
 
 export default reducer
