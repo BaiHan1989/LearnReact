5715cc575801419afe063a698708cd2b2e4622ae
Lazyload <baih_1989@163.com>
2020-12-29 16:46:21 +0800

    header的实现

--- hy-music-web/src/components/app-header/index.js
+++ hy-music-web/src/components/app-header/index.js
@@ -1,6 +1,10 @@
 import React, { memo } from 'react'
 
+import { headerLinks } from "@/common/local-data";
+
 import { NavLink } from 'react-router-dom'
+import { SearchOutlined } from "@ant-design/icons";
+import { Input } from "antd";
 import { 
   HeaderWrapper,
   HeaderLeft,
@@ -8,13 +12,42 @@ import {
 } from "./style";
 
 export default memo(function YMAppHeader() {
+
+  const showHeaderLinks = (item, index) => {
+    if (index < 3) {
+      return (
+        <NavLink to={item.link} exact>
+          {item.title}
+          <i className="icon sprite_01"></i>
+        </NavLink>
+      )
+    } else {
+      return <a href={item.link}>{item.title}</a>
+    }
+  }
+
   return (
     <HeaderWrapper>
       <div className="content wrap-v1">
         <HeaderLeft>
           <a href="#/" className="logo sprite_01"></a>
+          <div className="select-list">
+            {
+              headerLinks.map((item, index) => {
+                return (
+                  <div key={item.title} className="select-item">
+                    {showHeaderLinks(item, index)}
+                  </div>
+                )
+              })
+            }
+          </div>
         </HeaderLeft>
-        <HeaderRight>right</HeaderRight>
+        <HeaderRight>
+            <Input className="search" placeholder="音乐/视频/用户/电台" prefix={<SearchOutlined />} />
+            <div className="center">创作者中心</div>
+            <div className="login">登录</div>
+        </HeaderRight>
       </div>
       <div className="divider"></div>
     </HeaderWrapper>
