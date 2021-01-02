99f7a0d4e603c1c0f53ae2996a4e7b5172f3eab2
Lazyload <baih_1989@163.com>
2021-01-02 10:00:18 +0800

    redux的最基本的使用

--- 06_learn-redux/index.js
+++ 06_learn-redux/index.js
@@ -2,6 +2,7 @@
 
 const redux = require('redux')
 
+// 初始/默认状态
 const initialState = {
   counter: 0
 }
@@ -45,3 +46,18 @@ store.dispatch(action2)
 store.dispatch(action2)
 store.dispatch(action3)
 store.dispatch(action4)
+
+
+/**
+ * Redux 的核心理念
+ * store
+ * - 用来存储 state
+ * action 
+ * - 所有数据的变化，必须通过 dispatch(action) 来更新
+ * - action是一个普通的 JavaScript 对象，用来描述本次更新的 type 和 content
+ * reducer
+ * - 将 store 和 action 联系起来的桥梁
+ * - 是一个纯函数，函数内部不能对原始的 state 进行直接修改
+ * - 接收2个参数，第1个参数就是 state，第2个参数是 action
+ * - 根据 action 的 type，对 state 处理，返回新的 state
+ */
