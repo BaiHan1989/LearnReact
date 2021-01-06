0b8a6ffedda2a1c6b80fe94ba145d73ce5688b95
Lazyload <baih_1989@163.com>
2021-01-06 15:07:01 +0800

    reducer的拆分

--- 07_react-redux/src/store/reducer.js
+++ 07_react-redux/src/store/reducer.js
@@ -1,13 +1,18 @@
-
+import { combineReducers } from "redux";
 import { reducer as counterReducer } from "./counter";
 import { reducer as homeReducer } from "./home";
 
 
-function reducer(state = {}, action) {
-  return {
-    counterInfo: counterReducer(state.counterInfo, action),
-    homeInfo: homeReducer(state.homeInfo, action)
-  }
-}
+// function reducer(state = {}, action) {
+//   return {
+//     counterInfo: counterReducer(state.counterInfo, action),
+//     homeInfo: homeReducer(state.homeInfo, action)
+//   }
+// }
+
+const reducer = combineReducers({
+  counterInfo: counterReducer,
+  homeInfo: homeReducer
+})
 
 export default reducer
