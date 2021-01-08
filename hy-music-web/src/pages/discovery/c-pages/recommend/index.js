28a3fe87b6ef3a62facf3b6372fc30307929b440
Lazyload <baih_1989@163.com>
2021-01-08 14:41:31 +0800

    完成banner

--- hy-music-web/src/pages/discovery/c-pages/recommend/index.js
+++ hy-music-web/src/pages/discovery/c-pages/recommend/index.js
@@ -1,26 +1,15 @@
-import React, { memo, useEffect } from 'react'
-import { shallowEqual, useDispatch, useSelector } from 'react-redux'
-import { getTopBannerAction } from './store/actionCreators'
+import React, { memo } from 'react'
+
+import YMTopBanner from './c-cpns/top-banner'
+
+import { RecommendWrapper } from "./style";
 
 function YMRecommend(props) {
 
-  // redux 和 组件关联：获取数据和进行操作
-  const dispatch = useDispatch()
-
-  const { topBanners } = useSelector(state => ({
-    // topBanners: state.get("recommend").get("topBanners")
-    topBanners: state.getIn(["recommend", "topBanners"])
-  }), shallowEqual)
-
-  // 发送网络请求
-  useEffect(() => {
-    dispatch(getTopBannerAction())
-  }, [dispatch])
-
   return (
-    <div>
-      <h2>YMRecommend:{topBanners.length}</h2>
-    </div>
+    <RecommendWrapper>
+      <YMTopBanner />
+    </RecommendWrapper>
   )
 }
 
