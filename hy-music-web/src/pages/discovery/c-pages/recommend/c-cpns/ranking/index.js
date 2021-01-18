9521ecdbbf2738ef0941d6a08f09cb6dca2d098d
Lazyload <baih_1989@163.com>
2021-01-18 14:27:00 +0800

    排行榜页面

--- hy-music-web/src/pages/discovery/c-pages/recommend/c-cpns/ranking/index.js
+++ hy-music-web/src/pages/discovery/c-pages/recommend/c-cpns/ranking/index.js
@@ -1,14 +1,21 @@
 import React, { memo, useEffect } from 'react'
 
-import { useDispatch } from "react-redux";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
 
 import { getTopRankingAction } from "../../store/actionCreators";
 
-import YMThemeHeaderRCM from '@/components/theme-header-rcm'  
+import YMThemeHeaderRCM from '@/components/theme-header-rcm'
+import YMTopRanking from '@/components/top-ranking'
 
 import { RankingWrapper } from "./style";
 
 export default memo(function YMRanking() {
+
+  const { upRanking, newRanking, originRanking } = useSelector(state => ({
+    upRanking: state.getIn(["recommend", "upRanking"]),
+    newRanking: state.getIn(["recommend", "newRanking"]),
+    originRanking: state.getIn(["recommend", "originRanking"])
+  }), shallowEqual)
 
   const dispatch = useDispatch()
 
@@ -17,10 +24,15 @@ export default memo(function YMRanking() {
     dispatch(getTopRankingAction(2))
     dispatch(getTopRankingAction(3))
   }, [dispatch])
-  
+
   return (
     <RankingWrapper>
       <YMThemeHeaderRCM title="榜单" />
+      <div className="tops">
+        <YMTopRanking info={upRanking} />
+        <YMTopRanking info={newRanking} />
+        <YMTopRanking info={originRanking} />
+      </div>
     </RankingWrapper>
   )
 })
